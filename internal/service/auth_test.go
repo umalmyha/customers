bd41b752fffc20628c813167a1bcaef8e369448c
deus <deus@N158.ES.local>
2022-08-07 20:53:29 +0300

    fix lint

--- internal/service/auth_test.go
+++ internal/service/auth_test.go
@@ -3,6 +3,9 @@ package service
 import (
 	"context"
 	"crypto/ed25519"
+	"testing"
+	"time"
+
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/mock"
@@ -11,14 +14,13 @@ import (
 	"github.com/umalmyha/customers/internal/config"
 	"github.com/umalmyha/customers/internal/model"
 	"github.com/umalmyha/customers/internal/repository/mocks"
-	"testing"
-	"time"
 )
 
 const (
 	jwtAlgoEd25519 = "EdDSA"
 	jwtIssuerClaim = "test-issuer"
 	jwtTimeToLive  = 3 * time.Minute
+	jwtPrivateKey  = "MC4CAQAwBQYDK2VwBCIEIBvYJuek9MjwZuvYT+6W7S9RRgr0SmxRqejl2v6y9jjo"
 )
 
 const (
@@ -26,27 +28,15 @@ const (
 	refreshTokenTimeToLive = 720 * time.Hour
 )
 
-var testAuthCtx = context.Background()
-var testNow = time.Now().UTC()
-var testPassword = "secret_password"
-var testFingerprint = "87c37298-2f3d-40a1-9438-f45d2d819206"
-var testPrivateKey = ed25519.PrivateKey("MC4CAQAwBQYDK2VwBCIEIBvYJuek9MjwZuvYT+6W7S9RRgr0SmxRqejl2v6y9jjo")
-
-var jwtIssuer = auth.NewJwtIssuer(jwtIssuerClaim, jwt.GetSigningMethod(jwtAlgoEd25519), jwtTimeToLive, testPrivateKey)
-var rfrTokenCfg = &config.RefreshTokenCfg{MaxCount: refreshTokenMaxCount, TimeToLive: refreshTokenTimeToLive}
-
-var testUser = &model.User{
-	ID:           "bdf2f837-75f6-462a-b9ec-5dfb2e8f8792",
-	Email:        "test@email.com",
-	PasswordHash: "$2y$10$iKrALz6vQTs8KcAOElIdHeO0ZKWZkyfFnxPsJYU.Dys/2Rz177p32",
-}
-
-var testRfrToken = &model.RefreshToken{
-	ID:          "1165dfc0-2dd0-4bea-ac69-4462f1cacacf",
-	UserID:      testUser.ID,
-	Fingerprint: testFingerprint,
-	ExpiresIn:   int(refreshTokenTimeToLive.Seconds()),
-	CreatedAt:   testNow,
+type authTestData struct {
+	ctx         context.Context
+	now         time.Time
+	password    string
+	fingerprint string
+	issuer      *auth.JwtIssuer
+	user        *model.User
+	rfrToken    *model.RefreshToken
+	rfrTokenCfg *config.RefreshTokenCfg
 }
 
 type authServiceTestSuite struct {
@@ -55,6 +45,7 @@ type authServiceTestSuite struct {
 	transactorMock  *mocks.Transactor
 	userRpsMock     *mocks.UserRepository
 	rfrTokenRpsMock *mocks.RefreshTokenRepository
+	testData        *authTestData
 }
 
 func (s *authServiceTestSuite) SetupSuite() {
@@ -66,163 +57,241 @@ func (s *authServiceTestSuite) SetupSuite() {
 	).Return(func(ctx context.Context, txFunc func(ctx context.Context) error) error {
 		return txFunc(ctx)
 	})
+
+	now := time.Now().UTC()
+	fingerprint := "87c37298-2f3d-40a1-9438-f45d2d819206"
+	password := "secret_password"
+
+	jwtIssuer := auth.NewJwtIssuer(
+		jwtIssuerClaim,
+		jwt.GetSigningMethod(jwtAlgoEd25519),
+		jwtTimeToLive,
+		ed25519.PrivateKey(jwtPrivateKey),
+	)
+
+	user := &model.User{
+		ID:           "bdf2f837-75f6-462a-b9ec-5dfb2e8f8792",
+		Email:        "test@email.com",
+		PasswordHash: "$2y$10$iKrALz6vQTs8KcAOElIdHeO0ZKWZkyfFnxPsJYU.Dys/2Rz177p32",
+	}
+
+	rfrToken := &model.RefreshToken{
+		ID:          "1165dfc0-2dd0-4bea-ac69-4462f1cacacf",
+		UserID:      user.ID,
+		Fingerprint: fingerprint,
+		ExpiresIn:   int(refreshTokenTimeToLive.Seconds()),
+		CreatedAt:   now,
+	}
+
+	rfrTokenCfg := &config.RefreshTokenCfg{MaxCount: refreshTokenMaxCount, TimeToLive: refreshTokenTimeToLive}
+
+	s.testData = &authTestData{
+		ctx:         context.Background(),
+		now:         now,
+		password:    password,
+		fingerprint: fingerprint,
+		issuer:      jwtIssuer,
+		user:        user,
+		rfrToken:    rfrToken,
+		rfrTokenCfg: rfrTokenCfg,
+	}
 }
 
 func (s *authServiceTestSuite) SetupTest() {
 	t := s.T()
 	s.userRpsMock = mocks.NewUserRepository(t)
 	s.rfrTokenRpsMock = mocks.NewRefreshTokenRepository(t)
-	s.authSvc = NewAuthService(jwtIssuer, rfrTokenCfg, s.transactorMock, s.userRpsMock, s.rfrTokenRpsMock)
+	s.authSvc = NewAuthService(s.testData.issuer, s.testData.rfrTokenCfg, s.transactorMock, s.userRpsMock, s.rfrTokenRpsMock)
+	s.userRpsMock.TestData()
 }
 
 func (s *authServiceTestSuite) TestSignupEmailReserved() {
-	email := testUser.Email
+	ctx := s.testData.ctx
+	user := s.testData.user
+	email := s.testData.user.Email
+	password := s.testData.password
 
-	s.userRpsMock.On("FindByEmail", testAuthCtx, email).Return(testUser, nil).Once()
+	s.userRpsMock.On("FindByEmail", ctx, email).Return(user, nil).Once()
 
 	s.T().Logf("signup user %s, but email already reserved", email)
 	{
-		_, err := s.authSvc.Signup(testAuthCtx, email, testPassword)
+		_, err := s.authSvc.Signup(ctx, email, password)
 		s.Assert().Error(err, "user with email %s already exist but no error raised", email)
 		s.Assert().IsType(&echo.HTTPError{}, err, "error must be echo error")
 	}
 }
 
 func (s *authServiceTestSuite) TestSuccessfulSignup() {
-	email := testUser.Email
+	ctx := s.testData.ctx
+	email := s.testData.user.Email
+	password := s.testData.password
 
-	s.userRpsMock.On("FindByEmail", testAuthCtx, email).Return(nil, nil).Once()
-	s.userRpsMock.On("Create", testAuthCtx, mock.AnythingOfType("*model.User")).Return(nil).Once()
+	s.userRpsMock.On("FindByEmail", ctx, email).Return(nil, nil).Once()
+	s.userRpsMock.On("Create", ctx, mock.AnythingOfType("*model.User")).Return(nil).Once()
 
 	s.T().Logf("signup user %s and it must be signed up successfully", email)
 	{
-		_, err := s.authSvc.Signup(context.Background(), email, testPassword)
+		_, err := s.authSvc.Signup(context.Background(), email, password)
 		s.Assert().NoError(err, "user with email %s must be signed up successfully", email)
 	}
 }
 
 func (s *authServiceTestSuite) TestLoginBadUsername() {
-	email := testUser.Email
+	ctx := s.testData.ctx
+	email := s.testData.user.Email
+	fingerprint := s.testData.fingerprint
+	now := s.testData.now
+	password := s.testData.password
 
-	s.userRpsMock.On("FindByEmail", testAuthCtx, email).Return(nil, nil).Once()
+	s.userRpsMock.On("FindByEmail", ctx, email).Return(nil, nil).Once()
 
 	s.T().Logf("login user %s but email is not registered", email)
 	{
-		_, _, err := s.authSvc.Login(testAuthCtx, email, testPassword, testFingerprint, testNow)
+		_, _, err := s.authSvc.Login(ctx, email, password, fingerprint, now)
 		s.Assert().Error(err, "user with email %s is not registered, but no error raised", email)
 		s.Assert().ErrorIs(err, echo.ErrUnauthorized, "it must be unauthorized error")
 	}
 }
 
 func (s *authServiceTestSuite) TestLoginBadPassword() {
-	email := testUser.Email
+	ctx := s.testData.ctx
+	user := s.testData.user
+	email := s.testData.user.Email
+	fingerprint := s.testData.fingerprint
+	now := s.testData.now
 	invalidPassword := "invalid_password"
 
-	s.userRpsMock.On("FindByEmail", testAuthCtx, email).Return(testUser, nil).Once()
+	s.userRpsMock.On("FindByEmail", ctx, email).Return(user, nil).Once()
 
 	s.T().Logf("login user %s but password is incorrect", email)
 	{
-		_, _, err := s.authSvc.Login(testAuthCtx, email, invalidPassword, testFingerprint, testNow)
+		_, _, err := s.authSvc.Login(ctx, email, invalidPassword, fingerprint, now)
 		s.Assert().Error(err, "wrong password is provided but no error raised")
 		s.Assert().ErrorIs(err, echo.ErrUnauthorized, "it must be unauthorized error")
 	}
 }
 
 func (s *authServiceTestSuite) TestLoginSuccessAndPreviousTokensRemoved() {
-	email := testUser.Email
+	ctx := s.testData.ctx
+	user := s.testData.user
+	email := s.testData.user.Email
+	password := s.testData.password
+	fingerprint := s.testData.fingerprint
+	now := s.testData.now
 
 	dbTokens := []*model.RefreshToken{
 		{
 			ID:          "af1adce5-51a4-4d2e-a6ba-da0e7009a1bf",
-			UserID:      testUser.ID,
+			UserID:      user.ID,
 			Fingerprint: "86d36dcb-512b-402d-bec4-ae8922677cd7",
 			ExpiresIn:   1000,
-			CreatedAt:   testNow,
+			CreatedAt:   now,
 		},
 		{
 			ID:          "af1adce5-51a4-4d2e-a6ba-da0e7009a1bf",
-			UserID:      testUser.ID,
+			UserID:      user.ID,
 			Fingerprint: "88a6a8ac-1104-41ae-b13c-c33deb5af5c2",
 			ExpiresIn:   2000,
-			CreatedAt:   testNow,
+			CreatedAt:   now,
 		},
 	}
 
-	s.userRpsMock.On("FindByEmail", testAuthCtx, email).Return(testUser, nil).Once()
-	s.rfrTokenRpsMock.On("FindTokensByUserID", testAuthCtx, testUser.ID).Return(dbTokens, nil).Once()
-	s.rfrTokenRpsMock.On("DeleteByUserID", testAuthCtx, testUser.ID).Return(nil).Once()
-	s.rfrTokenRpsMock.On("Create", testAuthCtx, mock.AnythingOfType("*model.RefreshToken")).Return(nil).Once()
+	s.userRpsMock.On("FindByEmail", ctx, email).Return(user, nil).Once()
+	s.rfrTokenRpsMock.On("FindTokensByUserID", ctx, user.ID).Return(dbTokens, nil).Once()
+	s.rfrTokenRpsMock.On("DeleteByUserID", ctx, user.ID).Return(nil).Once()
+	s.rfrTokenRpsMock.On("Create", ctx, mock.AnythingOfType("*model.RefreshToken")).Return(nil).Once()
 
 	s.T().Logf("login user %s successfully, but all previous tokens will be removed", email)
 	{
-		jwToken, rfrToken, err := s.authSvc.Login(testAuthCtx, email, testPassword, testFingerprint, testNow)
+		jwToken, rfrToken, err := s.authSvc.Login(ctx, email, password, fingerprint, now)
 		s.Assert().NoError(err, "user login is correct but error was raised")
-		s.Assert().Equal(testNow.Add(jwtTimeToLive).Unix(), jwToken.ExpiresAt, "incorrect time to live was set for jwt")
+		s.Assert().Equal(now.Add(jwtTimeToLive).Unix(), jwToken.ExpiresAt, "incorrect time to live was set for jwt")
 		s.Assert().Equal(int(refreshTokenTimeToLive.Seconds()), rfrToken.ExpiresIn, "expires in is set incorrectly")
-		s.rfrTokenRpsMock.AssertCalled(s.T(), "DeleteByUserID", testAuthCtx, testUser.ID)
+		s.rfrTokenRpsMock.AssertCalled(s.T(), "DeleteByUserID", ctx, user.ID)
 	}
 }
 
 func (s *authServiceTestSuite) TestRefreshInvalidToken() {
-	s.rfrTokenRpsMock.On("FindByID", testAuthCtx, testRfrToken.ID).Return(nil, nil).Once()
+	ctx := s.testData.ctx
+	rfrToken := s.testData.rfrToken
+	fingerprint := s.testData.fingerprint
+	now := s.testData.now
+
+	s.rfrTokenRpsMock.On("FindByID", ctx, rfrToken.ID).Return(nil, nil).Once()
 
 	s.T().Log("refresh with invalid token")
 	{
-		_, _, err := s.authSvc.Refresh(testAuthCtx, testRfrToken.ID, testFingerprint, testNow)
+		_, _, err := s.authSvc.Refresh(ctx, rfrToken.ID, fingerprint, now)
 		s.Assert().Error(err, "invalid refresh token id was provided but no error raised")
 		s.Assert().IsType(&echo.HTTPError{}, err, "error must be echo error")
 	}
 }
 
 func (s *authServiceTestSuite) TestRefreshInvalidFingerprint() {
+	ctx := s.testData.ctx
+	rfrToken := s.testData.rfrToken
+	now := s.testData.now
 	invalidFingerprint := "461b07b5-3373-495d-b26b-d689a0c8a557"
 
-	s.rfrTokenRpsMock.On("FindByID", testAuthCtx, testRfrToken.ID).Return(testRfrToken, nil).Once()
-	s.rfrTokenRpsMock.On("DeleteByID", testAuthCtx, testRfrToken.ID).Return(nil).Once()
+	s.rfrTokenRpsMock.On("FindByID", ctx, rfrToken.ID).Return(rfrToken, nil).Once()
+	s.rfrTokenRpsMock.On("DeleteByID", ctx, rfrToken.ID).Return(nil).Once()
 
 	s.T().Log("refresh with invalid fingerprint")
 	{
-		_, _, err := s.authSvc.Refresh(testAuthCtx, testRfrToken.ID, invalidFingerprint, testNow)
+		_, _, err := s.authSvc.Refresh(ctx, rfrToken.ID, invalidFingerprint, now)
 		s.Assert().Error(err, "invalid refresh token fingerprint was provided but no error raised")
 		s.Assert().IsType(&echo.HTTPError{}, err, "error must be echo error")
 	}
 }
 
 func (s *authServiceTestSuite) TestRefreshExpiredToken() {
-	futureNow := testNow.Add(725 * time.Hour)
+	ctx := s.testData.ctx
+	rfrToken := s.testData.rfrToken
+	fingerprint := s.testData.fingerprint
+	now := s.testData.now
+	futureNow := now.Add(725 * time.Hour)
 
-	s.rfrTokenRpsMock.On("FindByID", testAuthCtx, testRfrToken.ID).Return(testRfrToken, nil).Once()
-	s.rfrTokenRpsMock.On("DeleteByID", testAuthCtx, testRfrToken.ID).Return(nil).Once()
+	s.rfrTokenRpsMock.On("FindByID", ctx, rfrToken.ID).Return(rfrToken, nil).Once()
+	s.rfrTokenRpsMock.On("DeleteByID", ctx, rfrToken.ID).Return(nil).Once()
 
 	s.T().Log("refresh with already expired token")
 	{
-		_, _, err := s.authSvc.Refresh(testAuthCtx, testRfrToken.ID, testFingerprint, futureNow)
+		_, _, err := s.authSvc.Refresh(ctx, rfrToken.ID, fingerprint, futureNow)
 		s.Assert().Error(err, "refresh for expired refresh token was provided but no error raised")
 		s.Assert().IsType(&echo.HTTPError{}, err, "error must be echo error")
 	}
 }
 
 func (s *authServiceTestSuite) TestRefreshSuccessful() {
-	s.rfrTokenRpsMock.On("FindByID", testAuthCtx, testRfrToken.ID).Return(testRfrToken, nil).Once()
-	s.rfrTokenRpsMock.On("DeleteByID", testAuthCtx, testRfrToken.ID).Return(nil).Once()
-	s.userRpsMock.On("FindByID", testAuthCtx, testRfrToken.UserID).Return(testUser, nil).Once()
-	s.rfrTokenRpsMock.On("Create", testAuthCtx, mock.AnythingOfType("*model.RefreshToken")).Return(nil).Once()
+	ctx := s.testData.ctx
+	user := s.testData.user
+	rfrToken := s.testData.rfrToken
+	fingerprint := s.testData.fingerprint
+	now := s.testData.now
+
+	s.rfrTokenRpsMock.On("FindByID", ctx, rfrToken.ID).Return(rfrToken, nil).Once()
+	s.rfrTokenRpsMock.On("DeleteByID", ctx, rfrToken.ID).Return(nil).Once()
+	s.userRpsMock.On("FindByID", ctx, rfrToken.UserID).Return(user, nil).Once()
+	s.rfrTokenRpsMock.On("Create", ctx, mock.AnythingOfType("*model.RefreshToken")).Return(nil).Once()
 
 	s.T().Log("refresh with already expired token")
 	{
-		jwToken, rfrToken, err := s.authSvc.Refresh(testAuthCtx, testRfrToken.ID, testFingerprint, testNow)
+		jwToken, newRfrToken, err := s.authSvc.Refresh(ctx, rfrToken.ID, fingerprint, now)
 		s.Assert().NoError(err, "refresh request is correctly sent but no error raised")
-		s.Assert().Equal(testNow.Add(jwtTimeToLive).Unix(), jwToken.ExpiresAt, "incorrect time to live was set for jwt")
-		s.Assert().Equal(int(refreshTokenTimeToLive.Seconds()), rfrToken.ExpiresIn, "expires in is set incorrectly")
+		s.Assert().Equal(now.Add(jwtTimeToLive).Unix(), jwToken.ExpiresAt, "incorrect time to live was set for jwt")
+		s.Assert().Equal(int(refreshTokenTimeToLive.Seconds()), newRfrToken.ExpiresIn, "expires in is set incorrectly")
 	}
 }
 
 func (s *authServiceTestSuite) TestLogout() {
-	s.rfrTokenRpsMock.On("DeleteByID", testAuthCtx, testRfrToken.ID).Return(nil).Once()
+	ctx := s.testData.ctx
+	rfrToken := s.testData.rfrToken
+
+	s.rfrTokenRpsMock.On("DeleteByID", ctx, rfrToken.ID).Return(nil).Once()
 
 	s.T().Log("refresh with already expired token")
 	{
-		err := s.authSvc.Logout(testAuthCtx, testRfrToken.ID)
+		err := s.authSvc.Logout(ctx, rfrToken.ID)
 		s.Assert().NoError(err, "logout request is correct but error was raised")
 	}
 }
--- internal/service/customer_test.go
+++ internal/service/customer_test.go
@@ -3,24 +3,18 @@ package service
 import (
 	"context"
 	"errors"
+	"testing"
+
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	cacheMocks "github.com/umalmyha/customers/internal/cache/mocks"
 	"github.com/umalmyha/customers/internal/model"
 	rpsMocks "github.com/umalmyha/customers/internal/repository/mocks"
-	"testing"
 )
 
-var testCustomerCtx = context.Background()
-
-var testCustomer = &model.Customer{
-	ID:         "ecc770d9-4576-4f72-affa-8b1454246692",
-	FirstName:  "John",
-	LastName:   "Walls",
-	MiddleName: nil,
-	Email:      "john.walls@somemal.com",
-	Importance: model.ImportanceCritical,
-	Inactive:   false,
+type customerTestData struct {
+	ctx      context.Context
+	customer *model.Customer
 }
 
 type customerServiceTestSuite struct {
@@ -28,6 +22,22 @@ type customerServiceTestSuite struct {
 	customerSvc       CustomerService
 	customerRpsMock   *rpsMocks.CustomerRepository
 	customerCacheMock *cacheMocks.CustomerCacheRepository
+	testData          *customerTestData
+}
+
+func (s *customerServiceTestSuite) SetupSuite() {
+	s.testData = &customerTestData{
+		ctx: context.Background(),
+		customer: &model.Customer{
+			ID:         "ecc770d9-4576-4f72-affa-8b1454246692",
+			FirstName:  "John",
+			LastName:   "Walls",
+			MiddleName: nil,
+			Email:      "john.walls@somemal.com",
+			Importance: model.ImportanceCritical,
+			Inactive:   false,
+		},
+	}
 }
 
 func (s *customerServiceTestSuite) SetupTest() {
@@ -38,110 +48,137 @@ func (s *customerServiceTestSuite) SetupTest() {
 }
 
 func (s *customerServiceTestSuite) TestFindByIDFromCache() {
-	s.customerCacheMock.On("FindByID", testCustomerCtx, testCustomer.ID).Return(testCustomer, nil).Once()
+	ctx := s.testData.ctx
+	customer := s.testData.customer
+
+	s.customerCacheMock.On("FindByID", ctx, customer.ID).Return(customer, nil).Once()
 
 	s.T().Log("customer must be found in cache")
 	{
-		_, err := s.customerSvc.FindByID(testCustomerCtx, testCustomer.ID)
+		_, err := s.customerSvc.FindByID(ctx, customer.ID)
 		s.Assert().NoError(err, "no error must be raised")
-		s.customerRpsMock.AssertNotCalled(s.T(), "FindByID", testCustomerCtx, testCustomer.ID)
+		s.customerRpsMock.AssertNotCalled(s.T(), "FindByID", ctx, customer.ID)
 	}
 }
 
 func (s *customerServiceTestSuite) TestFindByIDNotFound() {
-	s.customerCacheMock.On("FindByID", testCustomerCtx, testCustomer.ID).Return(nil, nil).Once()
-	s.customerRpsMock.On("FindByID", testCustomerCtx, testCustomer.ID).Return(nil, nil).Once()
+	ctx := s.testData.ctx
+	customer := s.testData.customer
+
+	s.customerCacheMock.On("FindByID", ctx, customer.ID).Return(nil, nil).Once()
+	s.customerRpsMock.On("FindByID", ctx, customer.ID).Return(nil, nil).Once()
 
 	s.T().Log("customer is missing in cache and in primary datasource")
 	{
-		customer, err := s.customerSvc.FindByID(testCustomerCtx, testCustomer.ID)
+		c, err := s.customerSvc.FindByID(ctx, customer.ID)
 		s.Assert().NoError(err, "no error must be raised")
-		s.Assert().Nil(customer, "no customer must be present but it was found")
+		s.Assert().Nil(c, "no customer must be present but it was found")
 		s.customerCacheMock.AssertNotCalled(s.T(), "Create", mock.AnythingOfType("*model.Customer"))
 	}
 }
 
 func (s *customerServiceTestSuite) TestFindByIDCached() {
-	s.customerCacheMock.On("FindByID", testCustomerCtx, testCustomer.ID).Return(nil, nil).Once()
-	s.customerRpsMock.On("FindByID", testCustomerCtx, testCustomer.ID).Return(testCustomer, nil).Once()
-	s.customerCacheMock.On("Create", testCustomerCtx, testCustomer).Return(nil).Once()
+	ctx := s.testData.ctx
+	customer := s.testData.customer
+
+	s.customerCacheMock.On("FindByID", ctx, customer.ID).Return(nil, nil).Once()
+	s.customerRpsMock.On("FindByID", ctx, customer.ID).Return(customer, nil).Once()
+	s.customerCacheMock.On("Create", ctx, customer).Return(nil).Once()
 
 	s.T().Log("customer is not in cache, found in primary datasource and cached")
 	{
-		customer, err := s.customerSvc.FindByID(testCustomerCtx, testCustomer.ID)
+		c, err := s.customerSvc.FindByID(ctx, customer.ID)
 		s.Assert().NoError(err, "no error must be raised")
-		s.Assert().NotNil(customer, "customer must be found")
-		s.customerCacheMock.AssertCalled(s.T(), "Create", testCustomerCtx, mock.AnythingOfType("*model.Customer"))
+		s.Assert().NotNil(c, "customer must be found")
+		s.customerCacheMock.AssertCalled(s.T(), "Create", ctx, mock.AnythingOfType("*model.Customer"))
 	}
 }
 
 func (s *customerServiceTestSuite) TestDeleteByIDCacheFailed() {
-	s.customerCacheMock.On("DeleteByID", testCustomerCtx, testCustomer.ID).Return(errors.New("cache err")).Once()
+	ctx := s.testData.ctx
+	customer := s.testData.customer
+
+	s.customerCacheMock.On("DeleteByID", ctx, customer.ID).Return(errors.New("cache err")).Once()
 
 	s.T().Log("delete customer from cache failed")
 	{
-		err := s.customerSvc.DeleteByID(testCustomerCtx, testCustomer.ID)
+		err := s.customerSvc.DeleteByID(ctx, customer.ID)
 		s.Assert().Error(err, "cache raised error - error must be raised up")
 	}
 }
 
 func (s *customerServiceTestSuite) TestDeleteByIDSuccessfully() {
-	s.customerCacheMock.On("DeleteByID", testCustomerCtx, testCustomer.ID).Return(nil).Once()
-	s.customerRpsMock.On("DeleteByID", testCustomerCtx, testCustomer.ID).Return(nil).Once()
+	ctx := s.testData.ctx
+	customer := s.testData.customer
+
+	s.customerCacheMock.On("DeleteByID", ctx, customer.ID).Return(nil).Once()
+	s.customerRpsMock.On("DeleteByID", ctx, customer.ID).Return(nil).Once()
 
 	s.T().Log("deleted successfully")
 	{
-		err := s.customerSvc.DeleteByID(testCustomerCtx, testCustomer.ID)
+		err := s.customerSvc.DeleteByID(ctx, customer.ID)
 		s.Assert().NoError(err, "no error must be raised")
-		s.customerRpsMock.AssertCalled(s.T(), "DeleteByID", testCustomerCtx, testCustomer.ID)
+		s.customerRpsMock.AssertCalled(s.T(), "DeleteByID", ctx, customer.ID)
 	}
 }
 
 func (s *customerServiceTestSuite) TestUpsertNewCustomer() {
-	s.customerRpsMock.On("FindByID", testCustomerCtx, testCustomer.ID).Return(nil, nil).Once()
-	s.customerRpsMock.On("Create", testCustomerCtx, mock.AnythingOfType("*model.Customer")).Return(nil).Once()
+	ctx := s.testData.ctx
+	customer := s.testData.customer
+
+	s.customerRpsMock.On("FindByID", ctx, customer.ID).Return(nil, nil).Once()
+	s.customerRpsMock.On("Create", ctx, mock.AnythingOfType("*model.Customer")).Return(nil).Once()
 
 	s.T().Log("user is not present, so must be created")
 	{
-		_, err := s.customerSvc.Upsert(testCustomerCtx, testCustomer)
+		_, err := s.customerSvc.Upsert(ctx, customer)
 		s.Assert().NoError(err, "no error must be raised")
-		s.customerRpsMock.AssertNotCalled(s.T(), "Update", testCustomerCtx, mock.AnythingOfType("*model.Customer"))
+		s.customerRpsMock.AssertNotCalled(s.T(), "Update", ctx, mock.AnythingOfType("*model.Customer"))
 	}
 }
 
 func (s *customerServiceTestSuite) TestUpsertUpdateCustomer() {
-	s.customerRpsMock.On("FindByID", testCustomerCtx, testCustomer.ID).Return(testCustomer, nil).Once()
-	s.customerCacheMock.On("DeleteByID", testCustomerCtx, testCustomer.ID).Return(nil).Once()
-	s.customerRpsMock.On("Update", testCustomerCtx, mock.AnythingOfType("*model.Customer")).Return(nil).Once()
+	ctx := s.testData.ctx
+	customer := s.testData.customer
+
+	s.customerRpsMock.On("FindByID", ctx, customer.ID).Return(customer, nil).Once()
+	s.customerCacheMock.On("DeleteByID", ctx, customer.ID).Return(nil).Once()
+	s.customerRpsMock.On("Update", ctx, mock.AnythingOfType("*model.Customer")).Return(nil).Once()
 
 	s.T().Log("user is present, so must be updated")
 	{
-		_, err := s.customerSvc.Upsert(testCustomerCtx, testCustomer)
+		_, err := s.customerSvc.Upsert(ctx, customer)
 		s.Assert().NoError(err, "no error must be raised")
-		s.customerRpsMock.AssertNotCalled(s.T(), "Create", testCustomerCtx, mock.AnythingOfType("*model.Customer"))
+		s.customerRpsMock.AssertNotCalled(s.T(), "Create", ctx, mock.AnythingOfType("*model.Customer"))
 	}
 }
 
 func (s *customerServiceTestSuite) TestCreateSuccessfully() {
-	s.customerRpsMock.On("Create", testCustomerCtx, testCustomer).Return(nil).Once()
+	ctx := s.testData.ctx
+	customer := s.testData.customer
+
+	s.customerRpsMock.On("Create", ctx, customer).Return(nil).Once()
 
 	s.T().Log("user must be created successfully")
 	{
-		_, err := s.customerSvc.Create(testCustomerCtx, testCustomer)
+		_, err := s.customerSvc.Create(ctx, customer)
 		s.Assert().NoError(err, "no error must be raised")
 	}
 }
 
 func (s *customerServiceTestSuite) TestFindAllSuccessfully() {
+	ctx := s.testData.ctx
+	customer := s.testData.customer
+
 	customers := []*model.Customer{
-		testCustomer,
+		customer,
 	}
 
-	s.customerRpsMock.On("FindAll", testCustomerCtx).Return(customers, nil).Once()
+	s.customerRpsMock.On("FindAll", ctx).Return(customers, nil).Once()
 
 	s.T().Log("users must be found from data source")
 	{
-		_, err := s.customerSvc.FindAll(testCustomerCtx)
+		_, err := s.customerSvc.FindAll(ctx)
 		s.Assert().NoError(err, "no error must be raised")
 	}
 }
